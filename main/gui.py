5ec93c0e79ccba51301ddafc7fee731b1f11fa29
ironstein <ironstein1994@gmail.com>
2015-12-19 18:31:12 +0530

    first commit
    added all the files from ironstein_mark2

---
+++ main/gui.py
@@ -0,0 +1,41 @@
+# import py_main
+
+# print("import complete")
+# print("khel shuru")
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.CURRENT_ARRAY = ['A','R','A','A']
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.modify_blocks()
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.CURRENT_ARRAY = ['D','H','R']
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.modify_blocks()
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.CURRENT_ARRAY = ['R','I']
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+
+# py_main.modify_blocks()
+
+# print(py_main.CURRENT_ARRAY_LENGTH)
+# print(py_main.CURRENT_ARRAY)
+import os
+working_directory= '/Users/ironstein/Documents/projects working directory/SCARA/DJ_Project_02/ironstein_mark2'
+os.listdir(working_directory)
+
+import subordinate
